0100deadc4cc08f2f18961898ebe20bbfe3dd589
mrmcnerd <blueark@outlook.com>
2017-04-28 07:43:59 +0100

    Moves some variables out of global

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
+"use strict";
 
-var preview = document.getElementById('preview');
-var previewctx = preview.getContext('2d');
-
-var MDown = false;
-var Color = '#f3a358';
-
+var app = {};
+(function(app){
+    app.canvas = document.getElementById('canvas');
+    app.context = app.canvas.getContext('2d');
+    app.preview = document.getElementById('preview');
+    app.previewctx = app.preview.getContext('2d');
+    app.mouse_down = false;
+    app.current_color = '#f3a358';
+    app.brush_size = 10;
+})(app);
 
 // Tools
 // -----------------------------------------------------------
 // COLOUR PALETTES
 var palette = document.getElementById('selectedcolor');
-palette.style.backgroundColor = Color;
+palette.style.backgroundColor = app.current_color;
 
 var brushcolors = document.getElementById('brushcolors');
-if (brushcolors.children.length > 0)
-{
-	var children = brushcolors.getElementsByTagName("div");
-	for (var i = 0; i < children.length; i++)
-	{
-		children[i].onclick = function() {
-			palette.style.backgroundColor = this.style.backgroundColor;
-			Color = palette.style.backgroundColor;
-		};
-	}
+if (brushcolors.children.length > 0) {
+  var children = brushcolors.getElementsByTagName("div");
+  for (var i = 0; i < children.length; i++) {
+    children[i].onclick = function () {
+      palette.style.backgroundColor = this.style.backgroundColor;
+      app.current_color = palette.style.backgroundColor;
+    };
+  }
 }
 
 // -----------------------------------------------------------
@@ -36,170 +37,164 @@ var saveImg = document.getElementById('saveimg');
 
 saveDiv.style.display = 'none';
 
-saveButton.onclick = function() {
-//	Canvas2Image.saveAsPNG(canvas);
-	var oImgPNG = Canvas2Image.saveAsPNG(canvas, true);
-	saveDiv.style.display = 'block';
-	
-	var dialogWidth = Math.max(canvas.width + 10, 200);
-	saveDiv.style.width = dialogWidth + "px";
-	
-	saveImg.innerHTML = '';
-	saveImg.appendChild(oImgPNG);
+saveButton.onclick = function () {
+  // Canvas2Image.saveAsPNG(canvas);
+  var oImgPNG = Canvas2Image.saveAsPNG(app.canvas, true);
+  saveDiv.style.display = 'block';
+
+  var dialogWidth = Math.max(app.canvas.width + 10, 200);
+  saveDiv.style.width = dialogWidth + "px";
+
+  saveImg.innerHTML = '';
+  saveImg.appendChild(oImgPNG);
 }
 
 var closeSaveFile = document.getElementById('closeSaveFile');
 
-closeSaveFile.onclick = function() {
-	saveDiv.style.display = 'none';
+closeSaveFile.onclick = function () {
+  saveDiv.style.display = 'none';
 }
 
 // -----------------------------------------------------------
 // FILL BUTTON
 var fillButton = document.getElementById('fillbutton');
-fillButton.onclick = function() {
-	context.rect(0,0,canvas.width, canvas.height);
-	context.fillStyle=Color;
-	context.fill();
+fillButton.onclick = function () {
+  app.context.rect(0, 0, app.canvas.width, app.canvas.height);
+  app.context.fillStyle = app.current_color;
+  app.context.fill();
 
-	previewctx.rect(0,0,preview.width, preview.height);
-	previewctx.fillStyle=Color;
-	previewctx.fill();
+  app.previewctx.rect(0, 0, app.preview.width, app.preview.height);
+  app.previewctx.fillStyle = app.current_color;
+  app.previewctx.fill();
 }
 
 // -----------------------------------------------------------
 // CLEAR BUTTON
 var clearButton = document.getElementById('clearbutton');
-clearButton.onclick = function() {
-	context.setTransform(1, 0, 0, 1, 0, 0);
-	context.clearRect(0, 0, canvas.width, canvas.height);
+clearButton.onclick = function () {
+  app.context.setTransform(1, 0, 0, 1, 0, 0);
+  app.context.clearRect(0, 0, app.canvas.width, app.canvas.height);
 
-	previewctx.setTransform(1, 0, 0, 1, 0, 0);
-	previewctx.clearRect(0, 0, preview.width, preview.height);
+  app.previewctx.setTransform(1, 0, 0, 1, 0, 0);
+  app.previewctx.clearRect(0, 0, app.preview.width, app.preview.height);
 }
 
 
 // -----------------------------------------------------------
-var curX = 0;	// current X co-ordinate
-var curY = 0;	// current Y co-ordinate
+var curX = 0; // current X co-ordinate
+var curY = 0; // current Y co-ordinate
 // Top left co-ordinates of canvas
 var canvasX0 = 0;
 var canvasY0 = 0;
 // Bottom right co-ordinates of canvas
-var canvasX1 = canvasX0 + canvas.width;
-var canvasY1 = canvasY0 + canvas.height;
+var canvasX1 = canvasX0 + app.canvas.width;
+var canvasY1 = canvasY0 + app.canvas.height;
 // Top left co-ordinates of canvas within window
-var winCanvasX = Position(canvas).left;
-var winCanvasY = Position(canvas).top;
+var winCanvasX = Position(app.canvas).left;
+var winCanvasY = Position(app.canvas).top;
 
-var cWidth = canvas.width;
-var cHeight = canvas.height;
-var pWidth = preview.width;
-var pHeight = preview.height;
+var cWidth = app.canvas.width;
+var cHeight = app.canvas.height;
+var pWidth = app.preview.width;
+var pHeight = app.preview.height;
 var pCenterStartX = (pWidth - cWidth) / 2;
 var pCenterStartY = (pHeight - cHeight) / 2;
-var pStartX = pCenterStartX - ((Math.ceil(pCenterStartX/cWidth)) * cWidth);
-var pStartY = pCenterStartY - ((Math.ceil(pCenterStartY/cHeight)) * cHeight);
+var pStartX = pCenterStartX - ((Math.ceil(pCenterStartX / cWidth)) * cWidth);
+var pStartY = pCenterStartY - ((Math.ceil(pCenterStartY / cHeight)) * cHeight);
 
-canvas.onselectstart = function() { return false; };
-canvas.unselectable = "on";
-canvas.style.MozUserSelect = "none";
+app.canvas.onselectstart = function () { return false; };
+app.canvas.unselectable = "on";
+app.canvas.style.MozUserSelect = "none";
 
 // -----------------------------------------------------------
 
-canvas.onmousedown = function(e) {
-	MDown = true;
-	context.strokeStyle = Color;
-	context.lineCap = 'round';
-	var winCanvasX = Position(canvas).left;
-	var winCanvasY = Position(canvas).top;
+app.canvas.onmousedown = function (e) {
+  app.mouse_down = true;
+  app.context.strokeStyle = app.current_color;
+  app.context.lineCap = 'round';
+  var winCanvasX = Position(app.canvas).left;
+  var winCanvasY = Position(app.canvas).top;
 
-	context.lineWidth = 3;
-	context.beginPath();
-	
-	curX = e.pageX - winCanvasX;
-	curY = e.pageY - winCanvasY;
-	
-	context.moveTo(curX, curY);
+  app.context.lineWidth = app.brush_size;
+  app.context.beginPath();
+
+  curX = e.pageX - winCanvasX;
+  curY = e.pageY - winCanvasY;
+
+  app.context.moveTo(curX, curY);
 }
 
 // -----------------------------------------------------------
 
-window.onmouseup = function()
-{
-	MDown = false;
-	UpdatePreview();
+window.onmouseup = function () {
+  app.mouse_down = false;
+  UpdatePreview();
 };
 
 // -----------------------------------------------------------
 
-window.onmousemove = function(e) { 
-	if (MDown) {
-		
-		var nextX = e.pageX - winCanvasX;
-		var nextY = e.pageY - winCanvasY;
-		
-		// Calculate if we go outside bounds of canvas
-		var newX1 = curX, newX2 = nextX, newY1 = curY, newY2 = nextY;
-		var reflect = false;
-		
-		if (nextX < canvasX0)
-		{
-			newX1 = curX + cWidth;
-			newX2 = nextX + cWidth;
-			reflect = true;
-		} else if (nextX > canvasX1) {
-			newX1 = curX - cWidth;
-			newX2 = nextX - cWidth;
-			reflect = true;
-		}
+window.onmousemove = function (e) {
+  if (app.mouse_down) {
 
-		if (nextY < canvasY0)
-		{
-			newY1 = curY + cHeight;
-			newY2 = nextY + cHeight;
-			reflect = true;
-		} else if (nextY > canvasY1) {
-			newY1 = curY - cHeight;
-			newY2 = nextY - cHeight;
-			reflect = true;
-		}
-		
-		context.lineTo(nextX, nextY);
-		if (reflect)
-		{
-			context.moveTo(newX1, newY1);
-			context.lineTo(newX2, newY2);
-		}
-		context.lineWidth = 3;
-		context.stroke();
-		context.moveTo(nextX, nextY);	// Replace position
-		
-		curX = nextX; curY = nextY;
-	}
+    var nextX = e.pageX - winCanvasX;
+    var nextY = e.pageY - winCanvasY;
+
+    // Calculate if we go outside bounds of canvas
+    var newX1 = curX, newX2 = nextX, newY1 = curY, newY2 = nextY;
+    var reflect = false;
+
+    if (nextX < canvasX0) {
+      newX1 = curX + cWidth;
+      newX2 = nextX + cWidth;
+      reflect = true;
+    } else if (nextX > canvasX1) {
+      newX1 = curX - cWidth;
+      newX2 = nextX - cWidth;
+      reflect = true;
+    }
+
+    if (nextY < canvasY0) {
+      newY1 = curY + cHeight;
+      newY2 = nextY + cHeight;
+      reflect = true;
+    } else if (nextY > canvasY1) {
+      newY1 = curY - cHeight;
+      newY2 = nextY - cHeight;
+      reflect = true;
+    }
+
+    app.context.lineTo(nextX, nextY);
+    if (reflect) {
+      app.context.moveTo(newX1, newY1);
+      app.context.lineTo(newX2, newY2);
+    }
+    app.context.lineWidth = app.brush_size;
+    app.context.stroke();
+    app.context.moveTo(nextX, nextY); // Replace position
+
+    curX = nextX; curY = nextY;
+  }
 }
 
 // -----------------------------------------------------------
 
 function Position(el) {
-	var position = {left: 0, top: 0};
-	if (el) {
-		if (!isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
-			position.left += el.offsetLeft;
-			position.top += el.offsetTop;
-		}
-	}
-	return position;
-}	
+  var position = { left: 0, top: 0 };
+  if (el) {
+    if (!isNaN(el.offsetLeft) && !isNaN(el.offsetTop)) {
+      position.left += el.offsetLeft;
+      position.top += el.offsetTop;
+    }
+  }
+  return position;
+}
 
 // -----------------------------------------------------------
 
 function UpdatePreview() {
-	for (var y=pStartY; y<preview.height; y+=cHeight)
-	{
-		for (var x=pStartX; x<preview.width; x+=cWidth)
-		{
-			previewctx.drawImage(canvas, x, y);
-		}
-	}
+  for (var y = pStartY; y < app.preview.height; y += cHeight) {
+    for (var x = pStartX; x < app.preview.width; x += cWidth) {
+      app.previewctx.drawImage(app.canvas, x, y);
+    }
+  }
 }
